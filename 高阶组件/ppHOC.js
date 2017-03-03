cc431b10b801047368dc282ce535fc2f3cfe2423
pingfengafei <intern@maxent-inc.com>
2017-03-03 12:04:35 +0800

    save webpack config

--- 高阶组件/ppHOC.js
+++ 高阶组件/ppHOC.js
@@ -1,16 +1,24 @@
 /**
  * Created by pingfengafei on 2/28/17.
  */
-import from 'react';
+//import React from 'react';
 
+var React = require('react');
+var WrappedComponent = require('./WrappedComponent');
+
+//操作props
 function ppHOC(WrappedComponent) {
-  //do proxy
-  
   return class PP extends React.Component {
     render() {
+      console.log('render');
       const newProps = {
         aaa: 123
       };
-      return <WrappedComponent {...this.props}/>;
+     // return <WrappedComponent {...this.props} {...newProps}/>;
     }
   }
+}
+
+var A = ppHOC(WrappedComponent);
+var b = new A();
+b.render();
